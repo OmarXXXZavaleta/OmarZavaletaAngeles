f3d1c90a880bbfe41cf37c3e82058db2f74310ae
OmarXXXZavaleta <74444097+OmarXXXZavaleta@users.noreply.github.com>
2021-04-13 09:12:21 -0500

    cammbios eb lugar donde se suben fotos

--- src/main/java/org/omar/zavaleta/config/WebConfig.java
+++ src/main/java/org/omar/zavaleta/config/WebConfig.java
@@ -8,8 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig  implements WebMvcConfigurer{
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
         //registry.addResourceHandler("/logos/**").addResourceLocations("file:/empleos/img-vacantes/"); // Linux
-        registry.addResourceHandler("/logos/**").addResourceLocations("file:c:/empleos/images_vacantes/"); // Windows
-	//registry.addResourceHandler("/logos/**").addResourceLocations("file:OmarXXXZavaleta/OmarZavaletaAngeles/tree/master/src/main/resources/static/images/");// eliminar si no sirve
+        //registry.addResourceHandler("/logos/**").addResourceLocations("file:c:/empleos/images_vacantes/"); // Windows
+	//registry.addResourceHandler("/logos/**").addResourceLocations("file:c:/images/");// eliminar si no sirve
 		
     }
 	
